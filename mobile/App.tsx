29da1060ec4b144f671fc8b5df244b6c6f89420a
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-09-20 22:47:39 -0300

    Assets adicionado - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,41 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-function Button (props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background';
+
 
 export default function App() {
   
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Oii</Text>
-      <StatusBar style="auto"/>
-      <Button title="Clique aqui"></Button>
-    </View>
+    <Background>
+     
+    </Background>
   );
 }
-
-interface ButtonProps {
-  title: string,
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'deeppink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 70,
-    fontWeight: 'bold',
-  }
-});
