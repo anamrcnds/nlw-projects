49d890cbed3840a7b52a01270032e386aa82e8c5
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-10-04 22:06:47 -0300

    Rotas adicionadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,19 @@
-import express from 'express';
+import express, { response } from 'express';
 const app = express();
-app.get('/ads', (request, response) => {
+
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id;
+
+    // return response.send(gameId);
+
     return response.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
@@ -10,6 +23,15 @@ app.get('/ads', (request, response) => {
         { id: 6, name: 'Anúncio 6' },
     ]);
 });
+
+app.get('/ads/:id/discord', (request, response) => {
+    const adId = request.params.id;
+
+    // return response.send(gameId);
+
+    return response.json([]);
+});
+
 app.listen(3333, () => {
     console.log('Servidor no ar!');
 });
